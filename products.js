7b5396540a4bb3eb8fa9f3dd8f62a803debeb404
Kievv <ievsouza622@gmail.com>
2022-11-28 20:11:48 -0300

    [finishes #183905786] Update List By Category

--- products.js
+++ products.js
@@ -1,34 +1,41 @@
 let scanf = require('scanf');
 
 const produtos = [
-    {
-        nome: "Notebook",
-        categoria: "Eletrônico",
-        valor: 1999.99
-    },
-    {
-        nome: "Impressora",
-        categoria: "Eletrônico",
-        valor: 999.99
-    },
-    {
-        nome: "Caneta BIC",
-        categoria: "Escolar",
-        valor: 0.50
-    },
-    {
-        nome: "Lapiseira Pentel",
-        categoria: "Escolar",
-        valor: 7.50
-    },
-]
+  {
+    nome: 'Notebook',
+    categoria: 'Eletrônico',
+    valor: 1999.99,
+  },
+  {
+    nome: 'Impressora',
+    categoria: 'Eletrônico',
+    valor: 999.99,
+  },
+  {
+    nome: 'Caneta BIC',
+    categoria: 'Escolar',
+    valor: 0.5,
+  },
+  {
+    nome: 'Lapiseira Pentel',
+    categoria: 'Escolar',
+    valor: 7.5,
+  },
+];
 
-
-function listar() {
-    produtos.forEach(p => {
-        console.log(p)
-    })
+function listar_por_categoria(cate) {
+  produtos.forEach((p) => {
+    if (p.categoria == cate) {
+      console.log(p);
+    }
+  });
 }
 
+function listar() {
+  produtos.forEach((p) => {
+    console.log(p);
+  });
+}
 
-
+const escolar = 'Escolar';
+listar_por_categoria(escolar);
